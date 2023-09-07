cfa92f8e23afa7210d5cb5ad3397c47412fe1249
__anuragdaksh7__ <anuragdaksh77777@gmail.com>
2023-09-07 23:17:19 +0530

    added monthly exp

---
+++ client/src/components/MonthlyExpenseOverview.js
@@ -0,0 +1,10 @@
+export const MonthlyExp = () => {
+    return (
+        <div>
+            <h1>Monthly Expense Overview</h1>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
